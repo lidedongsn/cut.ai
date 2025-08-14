b556eed53f9ecd76a4f56eb13381d035e80db3cb
lidedongsn <dongshining1990@163.com>
2025-08-14 17:28:49 +0800

    Add logo

--- www/web/src/components/FileUpload.vue
+++ www/web/src/components/FileUpload.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="p-6 w-96 mx-auto bg-white dark:bg-gray-700 text-gray-800 dark:text-white rounded-xl shadow-md flex items-center space-x-4 transition-colors duration-300">
+  <div
+    class="p-6 w-96 mx-auto bg-white dark:bg-gray-700 text-gray-800 dark:text-white rounded-xl shadow-md flex items-center space-x-4 transition-colors duration-300"
+  >
     <div class="flex flex-col w-full items-center">
+      <img src="@/assets/logo.png" alt="转写工具" class="absolute top-4 left-4 w-16 h-16" />
+
       <h2 class="mb-4 text-xl font-bold dark:text-gray-100">音视频文件上传</h2>
-      
+
       <input
         class="hidden"
         type="file"
@@ -10,25 +14,25 @@
         @change="handleFileChange"
         accept=".mp3, .wav, .m4a, .wma, .aac, .ogg, .amr, .flac, .mp4, .wmv, .m4v, .flv, .rmvb, .dat, .mov, .mkv, .webm, audio/aac"
       />
-      
+
       <span
-        class="truncate block w-full px-4 py-2 border border-dashed rounded-md cursor-pointer transition-colors duration-300 h-16
-               bg-gray-100 hover:bg-gray-200 
-               dark:bg-gray-900
-               dark:border-gray-600 dark:text-gray-200"
+        class="truncate block w-full px-4 py-2 border border-dashed rounded-md cursor-pointer transition-colors duration-300 h-16 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:border-gray-600 dark:text-gray-200"
         @click="triggerFileInput"
         :title="selectedFileName"
       >
         {{ selectedFileName || '选择音视频文件' }}
       </span>
-      
+
       <div v-if="isUploading" class="w-full bg-gray-300 dark:bg-gray-600 rounded-full h-2.5 mt-3">
-        <div :style="{ width: uploadProgress + '%' }" class="bg-emerald-500 dark:bg-emerald-400 h-2.5 rounded-full transition-all duration-300"></div>
+        <div
+          :style="{ width: uploadProgress + '%' }"
+          class="bg-emerald-500 dark:bg-emerald-400 h-2.5 rounded-full transition-all duration-300"
+        ></div>
       </div>
 
       <button
         class="w-full mt-4 py-2 transition-transform transform dark:bg-gray-900 hover:scale-105 hover:shadow-md rounded-lg text-white font-semibold border border-gray-600"
-         @click="uploadFile"
+        @click="uploadFile"
         :disabled="isUploading"
       >
         开始转写
@@ -86,13 +90,13 @@ export default {
           },
           timeout: 6000000
         })
-        
+
         if (response.status !== 200) {
           throw new Error('上传失败！')
         }
-        
+
         ElMessage.success('文件上传成功！')
-        
+
         // 发起STT任务
         const data = new FormData()
         data.append('file_id', response.data.data.file_id)
@@ -101,14 +105,14 @@ export default {
             'Content-Type': 'multipart/form-data'
           }
         })
-        
+
         if (sttResponse.status !== 200) {
           throw new Error('发起STT任务失败！')
         }
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise((resolve) => setTimeout(resolve, 1000))
 
-        this.$router.push(`/transcripts/${sttResponse.data.data.task_id}`); 
+        this.$router.push(`/transcripts/${sttResponse.data.data.task_id}`)
       } catch (error) {
         console.error('操作失败', error)
         ElMessage.error('操作失败，请重试！')
